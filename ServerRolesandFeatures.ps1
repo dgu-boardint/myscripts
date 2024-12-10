e3d621c556d1d7a421ce3a521aa5eb61d71179fe
dgu-boardint <129658926+dgu-boardint@users.noreply.github.com>
2024-12-10 12:47:34 +0100

    Add files via upload

---
+++ ServerRolesandFeatures.ps1
@@ -0,0 +1,25 @@
+Install-WindowsFeature -name Web-Server, Web-Basic-Auth, Web-Dyn-Compression, Web-Mgmt-Tools, Web-Asp-Net45, Web-ISAPI-Filter, Web-WebSockets
+##
+Install-WindowsFeature -name FS-Resource-Manager
+##
+Install-WindowsFeature -name Telnet-client
+##
+Start-Sleep -Seconds 5
+##
+Remove-IISSite -Name "Default Web Site" -Confirm:$false
+Remove-WebAppPool -Name "DefaultAppPool" -Confirm:$false
+##
+Import-Module WebAdministration
+Import-Module -Name IISAdministration
+New-WebAppPool -Name "B10WEB"
+New-WebAppPool -Name "B10ADMIN"
+Reset-IISServerManager -Confirm:$false
+$sm = Get-IISServerManager
+$web = Get-IISAppPool -name "B10WEB"
+$web.ProcessModel.loadUserProfile = $True
+Set-ItemProperty IIS:\AppPools\B10WEB -name Recycling.periodicRestart -value @{time = "00:00:00"}
+Set-ItemProperty IIS:\AppPools\B10WEB -name Recycling.periodicRestart.schedule -value @{value = "23:00"}
+Set-ItemProperty IIS:\AppPools\B10ADMIN -name ProcessModel -value @{IdentityType = "LocalSystem"}
+Set-ItemProperty IIS:\AppPools\B10ADMIN -name Recycling.periodicRestart -value @{time = "00:00:00"}
+Set-ItemProperty IIS:\AppPools\B10ADMIN -name Recycling.periodicRestart.schedule -value @{value = "22:55"}
+$sm.CommitChanges()
